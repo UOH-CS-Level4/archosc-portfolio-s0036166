f29327bf12e0a2bc9b9144fe9fdf0cc70fdcf1f0
KHALID ABOOD <K.ABOOD-2024@hull.ac.uk>
2026-02-17 12:01:40 +0000

    cmitting lab 4 work

---
+++ script.js
@@ -0,0 +1,9 @@
+const myName = document.querySelector(".name");
+const descriptionVariable = document.querySelector(".description");
+console.log(myName);
+console.log(descriptionVariable);
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
